076fb51c8c78050e2a24b7210aa9bdfcb2a700fa
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2013-07-07 13:46:10 +0200

    Add Pause and Reset buttons

--- src/test/java/com/pivotallabs/HomeActivityTest.java
+++ src/test/java/com/pivotallabs/HomeActivityTest.java
@@ -14,15 +14,29 @@ import static org.junit.Assert.assertThat;
 public class HomeActivityTest {
     private HomeActivity activity;
     private Button startButton;
+    private Button pauseButton;
+    private Button resetButton;
 
     @Before
     public void setUp() throws Exception {
         activity = Robolectric.buildActivity(HomeActivity.class).create().get();
         startButton = (Button) activity.findViewById(R.id.start_button);
+        pauseButton = (Button) activity.findViewById(R.id.pause_button);
+        resetButton = (Button) activity.findViewById(R.id.reset_button);
     }
 
     @Test
     public void shouldHaveStartButton() throws Exception {
         assertThat((String) startButton.getText(), equalTo("Start"));
+    }
+
+    @Test
+    public void shouldHavePauseButton() throws Exception {
+        assertThat((String) pauseButton.getText(), equalTo("Pause"));
+    }
+
+    @Test
+    public void shouldHaveResetButton() throws Exception {
+        assertThat((String) resetButton.getText(), equalTo("Reset"));
     }
 }
